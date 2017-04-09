1065e2e92a4735542e4852ec4abeed8b3098f880
PONTIFF\grandpancaker <grandpancaker@gmail.com>
2017-04-10 00:55:27 +0200

    splash

---
+++ lab4/splash.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace lab4
+{
+    partial class splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 66.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(75, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 101);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "666";
+            // 
+            // splash
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(333, 333);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "splash";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.splash_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.splash_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.splash_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ lab4/splash.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class splash : Form
+    {
+        Timer t = new Timer();
+        Timer close = new Timer();
+
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        public splash()
+        {
+            InitializeComponent();
+            Rectangle myEllipse = new Rectangle(0, 0, 333, 333);
+            GraphicsPath myPath = new GraphicsPath();
+            myPath.AddEllipse(myEllipse);
+
+            
+
+            this.Region = new Region(myPath);
+
+            t.Interval = 100;
+            t.Tick += new EventHandler(timer_Tick);
+            t.Start();
+            
+            close.Interval = 3000;
+            close.Tick += new EventHandler (HandleTimer);
+            close.Start();
+        }
+
+        void HandleTimer(object sender, EventArgs e)
+        {
+           this.Hide();
+
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.Opacity -= 0.1;
+            if (this.Opacity == 0)
+                this.Opacity = 1;
+        }
+
+        private void splash_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void splash_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void splash_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+    }
+}
